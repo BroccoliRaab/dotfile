a8435b1b21de7465bdda415885b408389e95e598
Linux User <robert@localhost.my.domain>
2021-09-28 20:03:27 -0400

    Added music controls

--- lembonbar/bar.sh
+++ lembonbar/bar.sh
@@ -23,8 +23,11 @@ volume(){
     vol="%{F#CCCCCC} $(pamixer --get-volume)"
     pamixer --get-mute > /dev/null && echo "$voldown$vol" || echo "$volup$vol"
 }
+song(){
+    mpc | sed 1q
+}
 while true ; do
-    bar="%{c}%{F#CCCCCC}$(clock)%{l}%{F#AE81FF}$(monitor)%{F#CCCCCC}:%{F#82B414}$(desktop) %{F#66D9Ef}$(window)%{r}%{F#Fd971F} %{F#CCCCCC}$(pkgs) %{F#f92672}$(volume)"
+    bar="%{c}%{F#CCCCCC}$(clock)%{l}%{F#AE81FF}$(monitor)%{F#CCCCCC}:%{F#82B414}$(desktop) %{F#66D9Ef}$(window)%{r}%{F#82b414}$(song)  %{F#Fd971F} %{F#CCCCCC}$(pkgs) %{F#f92672}$(volume)"
     echo "%{S0}$bar%{S1}$bar"
     sleep 1
 done |lemonbar -p -B#1b1d1e -F#cccccc -f "firamono-11" -f 'Font Awesome 5 Free Solid-12' 
